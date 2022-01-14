b822c8e523535dfda13464d8eca16b06b0f4c399
brent <brenton.j.andrews@gmail.com>
2022-01-14 09:34:12 -0700

    Game driver script refactor in progress

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// domSelect module -> select all dom elements from html for use by other modules.
+let domSelect = (function() {
+    let info_text = document.getElementById("game-info");
+    let game_squares = Array.from(document.getElementsByClassName("game_square"));
+    let startBtn = document.getElementById("start-btn");
+
+    return {
+        info_text: info_text,
+        game_squares: game_squares,
+        startBtn: startBtn
+    }
+})();
+
+
 // gameState module -> Stores information about the current game state.
 let gameState = (function() {
 
@@ -22,55 +36,7 @@ let gameState = (function() {
 })();
 
 
-// gameDisplay module -> Displays gameState._game_board on webpage, controls game flow.
-let singleGameFlow = (function() {
-    let _html_game_squares = Array.from(document.getElementsByClassName("game_square"));
-    let player1 = "x";
-    let player2 = "o";
-    let isX = true;
-
-    function fillBoard() {
-        let i = 0;
-        _html_game_squares.forEach(square => {
-            square.textContent = gameState.game_board[i];
-            square.addEventListener("click", () => {
-                if (winningConditions.checkLegalMove(square.id)) {
-
-                    // Fill square / array.
-                    if (isX) { 
-                        square.textContent = player1; 
-                        gameState.addMove(square.id, player1);
-                    }
-                    else {
-                        square.textContent = player2; 
-                        gameState.addMove(square.id, player2);
-                    }
-                    
-
-                    // Check winning conditions, toggle player turn.
-                    winningConditions.checkWinningConditions();
-                    togglePlayerTurn();
-                }
-
-            });
-            i++;
-        })
-    }
-
-    function togglePlayerTurn() {
-        isX = !isX;
-        return isX;
-    }
-
-    return {
-        fillBoard: fillBoard,
-        isX: isX
-    }
-
-})();
-
-
-// winningConditions checks for move legality, passes legal moves to gameState, calls publicFillBoard() and checks if winning conditions present.
+// winningConditions module checks for move legality, passes legal moves to gameState, and checks if winning conditions present.
 const winningConditions = (function() {
 
     let winningArr = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
@@ -85,32 +51,94 @@ const winningConditions = (function() {
     }
 
     function checkWinningConditions() {
-        console.log("next iteration...")
-        winningArr.forEach(condition => {
-            let winner = (gameState.game_board[condition[0]] === gameState.game_board[condition[1]]) && (gameState.game_board[condition[1]] === gameState.game_board[condition[2]] && gameState.game_board[condition[0]] != "");
-            if (winner) {
-                console.log("you win!");
-                return true;
-            }
-        })
+        for (let i = 0; i < winningArr.length; i++) {
+            let a = gameState.game_board[winningArr[i][0]];
+            let b = gameState.game_board[winningArr[i][1]];
+            let c = gameState.game_board[winningArr[i][2]];
+            let winner = (a === b && b === c && c != "");
+            if (winner) return true;
+        }
+
+        return false;
     }
     
     return {
         checkLegalMove: checkLegalMove,
-        checkWinningConditions: checkWinningConditions
+        checkWinningConditions: checkWinningConditions,
     }
 
 })();
 
 
-// player factory function -> Returns player object containing player name, wins, loses.
-const playerFactory = (name, symbol) => {
-    let character = symbol;
-    let wins = 0;
-    let loses = 0;
-    let stats = () => console.log(name + " has " + wins + " wins and " + loses + " loses.");
-    return { character, name, wins, loses, stats }
-};
+// singleGameFlow module -> Logic for game flow of one game of tic tac toe.
+let singleGameFlow = (function() {
+    let player1 = "x";
+    let player2 = "o";
+    let isX = false;
+    let gameWon = null;
 
 
-singleGameFlow.fillBoard();
+    // Add gameState array contents to display, add event listener for each square.
+    function fillBoard() {
+        let i = 0;
+        togglePlayerTurn();
+        domSelect.game_squares.forEach(square => {
+            square.textContent = gameState.game_board[i];
+            square.addEventListener("click", function() {
+                gameDriver(square);
+            });
+
+            i++;
+        });
+    }
+
+    // Game script function.
+    function gameDriver(square) {
+        if (winningConditions.checkLegalMove(square.id)) {
+
+            // Fill square / array.
+            if (isX) { 
+                square.textContent = player1; 
+                gameState.addMove(square.id, player1);
+            }
+            else {
+                square.textContent = player2; 
+                gameState.addMove(square.id, player2);
+            }
+            
+    
+            // Check winning conditions. 
+            if (winningConditions.checkWinningConditions()) {
+                if (isX === true) {
+                     domSelect.info_text.textContent = "X is the winner!";
+                } else domSelect.info_text.textContent = "O is the winner!";
+                gameWon = true;
+            }
+            
+            if (!gameWon) togglePlayerTurn();
+        }
+    }
+
+    // Toggle player turn and change information text above the board.
+    function togglePlayerTurn() {
+        isX = !isX;
+        isX === true ? domSelect.info_text.textContent = "X is up" : domSelect.info_text.textContent = "0 is up";
+        return isX;
+    }
+
+    return {
+        fillBoard: fillBoard,
+        isX: isX,
+        gameWon: gameWon
+    }
+
+})();
+
+
+// programDriver module -> Initializes score and player objects, score display and calls singleGameFlow() to start another game.
+let programDriver = (function() {
+    domSelect.info_text.textContent = "Press the start button"
+    domSelect.startBtn.addEventListener("click", singleGameFlow.fillBoard, false);
+});
+
+programDriver();
