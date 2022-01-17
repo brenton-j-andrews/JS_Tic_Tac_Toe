03b59ca100f5593b2cc7da84cff72668e67c99a0
brent <brenton.j.andrews@gmail.com>
2022-01-17 09:03:44 -0700

    Single game feature works, need to clean up code

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 let domSelect = (function() {
     let info_text = document.getElementById("game-info");
     let game_squares = Array.from(document.getElementsByClassName("game_square"));
+    let game_container = document.getElementById("game_container");
     let startBtn = document.getElementById("start-btn");
 
     return {
         info_text: info_text,
         game_squares: game_squares,
-        startBtn: startBtn
+        startBtn: startBtn,
+        game_container: game_container
     }
 })();
-
 
 // gameState module -> Stores information about the current game state.
 let gameState = (function() {
@@ -24,17 +25,15 @@ let gameState = (function() {
     function addMove(move, marker) {
         game_board[move] = marker;
         console.log(game_board);
-
     }
 
     return {
         game_board: game_board,
         resetBoard: resetBoard,
         addMove: addMove
-    };
+    }
    
 })();
-
 
 // winningConditions module checks for move legality, passes legal moves to gameState, and checks if winning conditions present.
 const winningConditions = (function() {
@@ -43,11 +42,16 @@ const winningConditions = (function() {
 
     // Return true if move is legal.
     function checkLegalMove(move) {
-        if (gameState.game_board[move] === "") {
-            return true;
-        } else {
-            return false;
+        return gameState.game_board[move] === "" ? true : false;
+    }
+
+    function isTie() {
+        let emptySquares = 0;
+        for (let i = 0; i < 8; i++) {
+            if (gameState.game_board[i] === "") emptySquares++;
         }
+        
+        return (emptySquares === 0) ? true : false;
     }
 
     function checkWinningConditions() {
@@ -58,38 +62,42 @@ const winningConditions = (function() {
             let winner = (a === b && b === c && c != "");
             if (winner) return true;
         }
-
         return false;
     }
     
     return {
         checkLegalMove: checkLegalMove,
         checkWinningConditions: checkWinningConditions,
+        isTie: isTie
     }
 
 })();
-
 
 // singleGameFlow module -> Logic for game flow of one game of tic tac toe.
 let singleGameFlow = (function() {
     let player1 = "x";
     let player2 = "o";
     let isX = false;
-    let gameWon = null;
-
-
-    // Add gameState array contents to display, add event listener for each square.
+    let gameOver = false;
+    
+    // Add gameState array contents to display, add "click" event listener for each square.
     function fillBoard() {
-        let i = 0;
-        togglePlayerTurn();
-        domSelect.game_squares.forEach(square => {
-            square.textContent = gameState.game_board[i];
-            square.addEventListener("click", function() {
-                gameDriver(square);
+        if (!gameOver) {
+            console.log("test...")
+            let i = 0;
+            togglePlayerTurn();
+            domSelect.startBtn.disabled = true;
+            domSelect.game_squares.forEach(square => {
+                square.textContent = gameState.game_board[i];
+                square.addEventListener("click", listener, true);
+                i++;
             });
+        }
+    }
 
-            i++;
-        });
+    // EventListener helper function.
+    function listener() {
+        gameDriver(this);
     }
 
     // Game script function.
@@ -106,16 +114,26 @@ let singleGameFlow = (function() {
                 gameState.addMove(square.id, player2);
             }
             
-    
             // Check winning conditions. 
             if (winningConditions.checkWinningConditions()) {
                 if (isX === true) {
-                     domSelect.info_text.textContent = "X is the winner!";
-                } else domSelect.info_text.textContent = "O is the winner!";
-                gameWon = true;
+                     domSelect.info_text.innerText = "X is the winner! \nPress Start to play again!";
+                } else domSelect.info_text.innerText  = "O is the winner! \nPress Start to play again!";
+                gameOver = true;
             }
-            
-            if (!gameWon) togglePlayerTurn();
+
+            // Check for a drawn game.
+            if (winningConditions.isTie()) {
+                domSelect.info_text.innerText  = "It is a tie! \nPress Start to play again!";
+            }
+
+            // If game won, remove square eventListeners and return to programDriver function.
+            if (gameOver) {
+                domSelect.game_container.classList.add("game_over");
+                square.removeEventListener("click", listener, true);
+            }
+
+            if (!gameOver) togglePlayerTurn();
         }
     }
 
@@ -123,22 +141,20 @@ let singleGameFlow = (function() {
     function togglePlayerTurn() {
         isX = !isX;
         isX === true ? domSelect.info_text.textContent = "X is up" : domSelect.info_text.textContent = "0 is up";
-        return isX;
+
     }
 
     return {
         fillBoard: fillBoard,
         isX: isX,
-        gameWon: gameWon
+        gameOver: gameOver
     }
-
 })();
-
 
 // programDriver module -> Initializes score and player objects, score display and calls singleGameFlow() to start another game.
 let programDriver = (function() {
-    domSelect.info_text.textContent = "Press the start button"
-    domSelect.startBtn.addEventListener("click", singleGameFlow.fillBoard, false);
+    domSelect.info_text.textContent = "Press the start button";
+    domSelect.startBtn.addEventListener("click", singleGameFlow.fillBoard);
 });
 
 programDriver();
